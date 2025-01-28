487e71942527e030582666f54a6fc2d98279f18f
SKEGDEV <skegdeveloper@gmail.com>
2025-01-27 18:48:14 -0600

    v1.0 initial

---
+++ KeyLibraryMGP/CryptoLibrary.cs
@@ -0,0 +1,95 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace KeyLibraryMGP
+{
+    public class CryptoLibrary
+    {
+        /// <summary>
+        /// Encript text most used for systems keys as DB conection string, api keys, etc...
+        /// </summary>
+        /// <param name="TextPlain">text that can encrypt</param>
+        /// <returns>text plain encripted based in the entry text plain</returns>
+        /// <exception cref="Exception">Thrown if something of the process is wrong</exception>
+        /// <example>
+        /// <code>
+        /// CryptoKey crypto = new CryptoKey();
+        /// string encryptedKey = crypto.Encrypt("hello world");
+        /// Console.WriteLine(encryptedKey); // Output: "qHRYdTQf4rxaRHydExeNhw=="
+        /// </code>
+        /// </example>
+        public string Encript(string TextPlain)
+        { 
+            try
+            {
+                string result = string.Empty;
+                string secretKey = "santhosh";
+                string publicKey = "engineer";
+                byte[] secretByte = { };
+                secretByte = System.Text.Encoding.UTF8.GetBytes(secretKey);
+                byte[] publicByte = { };
+                publicByte = System.Text.Encoding.UTF8.GetBytes(publicKey);
+                byte[] inputByte = System.Text.Encoding.UTF8.GetBytes(TextPlain);
+                MemoryStream ms = null;
+                CryptoStream cs = null;
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    ms = new MemoryStream();
+                    cs = new CryptoStream(ms, des.CreateEncryptor(publicByte, secretByte), CryptoStreamMode.Write);
+                    cs.Write(inputByte, 0, inputByte.Length);
+                    cs.FlushFinalBlock();
+                    result = Convert.ToBase64String(ms.ToArray());
+                }
+                return result;
+            }
+            catch (Exception e)
+            {
+                return $"Error: {e.Message}";
+            }
+        }
+        /// <summary>
+        /// Encript text most used for systems keys as DB conection string, api keys, etc...
+        /// </summary>
+        /// <param name="EncriptedText">text that can decrypt</param>
+        /// <returns>text plain based in encripted text entry</returns>
+        /// <exception cref="Exception">Thrown if something of the process is wrong</exception>
+        /// <example>
+        /// <code>
+        /// CryptoKey crypto = new CryptoKey();
+        /// string DencryptedKey = crypto.Decrypt("qHRYdTQf4rxaRHydExeNhw==");
+        /// Console.WriteLine(DencryptedKey); // Output: "hello world"
+        /// </code>
+        /// </example>
+        public string Decrypt(string EncriptedText)
+        {
+            try
+            {
+                string result = string.Empty;
+                string secretKey = "santhosh";
+                string publicKey = "engineer";
+                byte[] secretByte = { };
+                secretByte = System.Text.Encoding.UTF8.GetBytes(secretKey);
+                byte[] publicByte = { };
+                publicByte = System.Text.Encoding.UTF8.GetBytes(publicKey);
+                byte[] inputByte = new byte[EncriptedText.Replace(" ", "+").Length];
+                inputByte = Convert.FromBase64String(EncriptedText.Replace(" ", "+"));
+                MemoryStream ms = null;
+                CryptoStream cs = null;
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    ms = new MemoryStream();
+                    cs = new CryptoStream(ms, des.CreateDecryptor(publicByte, secretByte), CryptoStreamMode.Write);
+                    cs.Write(inputByte, 0, inputByte.Length);
+                    cs.FlushFinalBlock();
+                    Encoding encoding = Encoding.UTF8;
+                    result = encoding.GetString(ms.ToArray());
+                }
+                return result;
+            }
+            catch (Exception e)
+            {
+                return $"Error: {e.Message}";
+            }
+        }
+    }
+}
---
+++ KeyLibraryMGP/KeyLibrary.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyLibraryMGP
+{
+    public class KeyLibrary : CryptoLibrary
+    {
+        /// <summary>
+        /// Encript text most used for systems keys as DB conection string, api keys, etc...
+        /// </summary>
+        /// <param name="EncriptedText">text that can decrypt</param>
+        /// <returns>text plain based in encripted text entry</returns>
+        /// <exception cref="Exception">Thrown if something of the process is wrong</exception>
+        /// <example>
+        /// <code>
+        /// CryptoKey crypto = new CryptoKey();
+        /// string DencryptedKey = crypto.Decrypt("qHRYdTQf4rxaRHydExeNhw==");
+        /// Console.WriteLine(DencryptedKey); // Output: "hello world"
+        /// </code>
+        /// </example>
+        public string getKey(string keyName)
+        {
+            RegistryKey machineKeys = null;
+            string result = string.Empty;
+            try
+            {
+                machineKeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\MGP");
+                result = machineKeys.GetValue(keyName).ToString();
+                return this.Decrypt(result);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    machineKeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\MGP");
+                    result = machineKeys.GetValue(keyName).ToString();
+                    return this.Decrypt(result);
+                }
+                catch (Exception e) { return $"Error: {e.Message}"; }
+            }
+            finally
+            {
+                machineKeys.Close();
+            }
+        }
+    }
+}
